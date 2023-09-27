c5dfbf1c6b045708ef3be3d4677ad1312497d7f2
Cristiano Biral Jorge <cristiano.biral.jorge@gmail.com>
2023-09-27 20:02:47 -0300

    notification worker

--- app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
@@ -1,12 +1,15 @@
 package me.dio.copa.catar.features
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import dagger.hilt.android.AndroidEntryPoint
+import me.dio.copa.catar.extensions.observe
+import me.dio.copa.catar.notification.scheduler.extensions.NotificationMatchesWorker
 import me.dio.copa.catar.ui.theme.Copa2022Theme
 
 @AndroidEntryPoint
@@ -16,10 +19,24 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        observeActions()
         setContent {
             Copa2022Theme {
                 val state by viewModel.state.collectAsState()
-                MainScreen(matches = state.matches)
+                MainScreen(matches = state.matches, viewModel::toggleNotification)
+            }
+        }
+    }
+
+    private fun observeActions() {
+        viewModel.action.observe(this) { action ->
+            when(action) {
+                is MainUiAction.DisableNotification ->
+                    NotificationMatchesWorker.cancel(applicationContext, action.match)
+                is MainUiAction.EnableNotification ->
+                    NotificationMatchesWorker.start(applicationContext, action.match)
+                is MainUiAction.MatchesNotFound -> Log.e("ACTION", "nenhuma partida encontrada")
+                MainUiAction.Unexpected -> Log.e("ACTION", "erro")
             }
         }
     }
--- app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
@@ -6,8 +6,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.dio.copa.catar.core.BaseViewModel
+import me.dio.copa.catar.domain.model.Match
 import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.usecase.DisableNotificationUseCase
+import me.dio.copa.catar.domain.usecase.EnableNotificationUseCase
 import me.dio.copa.catar.domain.usecase.GetMatchesUseCase
 import me.dio.copa.catar.remote.NotFoundException
 import me.dio.copa.catar.remote.UnexpectedException
@@ -15,7 +19,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getMatchesUseCase: GetMatchesUseCase
+    private val getMatchesUseCase: GetMatchesUseCase,
+    private val disableNotificationUseCase: DisableNotificationUseCase,
+    private val enableNotificationUseCase: EnableNotificationUseCase,
 ) : BaseViewModel<MainUiState, MainUiAction>(MainUiState()) {
 
     init {
@@ -39,6 +45,26 @@ class MainViewModel @Inject constructor(
             }
     }
 
+    fun toggleNotification(match: Match) {
+        viewModelScope.launch {
+            runCatching {
+                withContext(Dispatchers.Main) {
+
+                    val action = if (match.notificationEnabled) {
+                        disableNotificationUseCase(match.id)
+                        MainUiAction.DisableNotification(match)
+                    } else {
+                        enableNotificationUseCase(match.id)
+                        MainUiAction.EnableNotification(match)
+                    }
+
+                    sendAction(action)
+
+                }
+            }
+        }
+    }
+
 }
 
 data class MainUiState(
@@ -48,4 +74,6 @@ data class MainUiState(
 sealed class MainUiAction {
     object Unexpected: MainUiAction()
     data class MatchesNotFound(val message: String) : MainUiAction()
+    data class EnableNotification(val match: MatchDomain) : MainUiAction()
+    data class DisableNotification(val match: MatchDomain) : MainUiAction()
 }
